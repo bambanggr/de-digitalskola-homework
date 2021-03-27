3e5fbb090d7883d608e6e6bd2789c695d2bfe209
Bambang Raharjo <bambanggr@gmail.com>
2021-03-27 22:20:00 +0700

    package and main

---
+++ session11/src/etl/main.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+current_dir = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(os.path.abspath(current_dir + "/../service"))
+
+from operand import Operand
+
+if __name__ == "__main__":
+    # obj_operand = Operand(2,10,'kuadrat')
+    # obj_operand = Operand(2,10,'plus1')
+    obj_operand = Operand(2,10,'fibonacci')
+    # obj_operand.fnc_kuadrat()
+    # print(obj_operand)
+    # print(obj_operand.fnc_plus1())
+    # print(obj_operand.fnc_kuadrat())
+    # print(obj_operand.fnc_fibonacci())
